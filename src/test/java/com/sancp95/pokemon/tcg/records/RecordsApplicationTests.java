886fb9e32c63250e9fdc14cf96af186fda9875f0
Santiago Correa Perez <sancp1995@gmail.com>
2023-11-25 09:05:00 -0500

    Base project

---
+++ src/test/java/com/sancp95/pokemon/tcg/records/RecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sancp95.pokemon.tcg.records;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
